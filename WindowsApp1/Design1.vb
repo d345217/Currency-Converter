348b09a38dc064e179a12dfcc54afda77a2690e1
11155882@college.wlc.ac.uk <11155882@college.wlc.ac.uk>
2023-03-14 14:39:26 +0000

    -added commenting

--- WindowsApp1/Design1.vb
+++ WindowsApp1/Design1.vb
@@ -1,48 +1,70 @@
 ﻿Public Class Design1
+
+    'variable validation
     Dim Amount As Double
     Dim ConversionRate As Double
     Dim Total As Double
     ReadOnly array(4, 1) As String
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
         ArrayCreate()
 
+        'filling listboxes with country names
         For i = 0 To 4
             lstCountries.Items.Add(array(i, 0))
         Next
 
+        'adding history to lstEntries
         Dim lines() As String = IO.File.ReadAllLines("C:\Users\11155882\Source\Repos\d345217\Currency-Converter\WindowsApp1\output1.txt")
         lstEntries1.Items.AddRange(lines)
+
     End Sub
+
     Private Sub btnCalculate1_Click(sender As Object, e As EventArgs) Handles btnCalculate1.Click
+
+        'input validation
         If Not ValidateInput() Then
             Exit Sub
         End If
 
         Calculate(lstCountries.SelectedIndex)
+
     End Sub
+
     Private Sub ArrayCreate()
+
         'adding countries
         array(0, 0) = "United Kingdom"
         array(1, 0) = "Ukraine"
         array(2, 0) = "USA"
         array(3, 0) = "Poland"
         array(4, 0) = "Euro"
+
         'adding change rate to Hryvna
         array(0, 1) = 1
         array(1, 1) = 44.66
         array(2, 1) = 1.21
         array(3, 1) = 5.3
         array(4, 1) = 1.13
+
     End Sub
+
     Private Sub Calculate(ByRef x As Integer)
+
+        'calculations of conversion
         ConversionRate = array(x, 1)
         Amount = txtAmount.Text
         Total = Amount * ConversionRate
 
+        'output to textbox
         txtTotal.Text = Math.Round(Total, 2)
         txtRate.Text = Math.Round(ConversionRate, 2)
+
     End Sub
+
     Private Function ValidateInput() As Boolean
+
         'check if amount is a positive number or exist
         If Not Double.TryParse(txtAmount.Text, Amount) OrElse Amount < 0 Then
             MessageBox.Show("Please enter a valid positive number for amount.")
@@ -56,17 +78,24 @@
         End If
 
         Return True
+
     End Function
 
+
     Private Sub btnSave1_Click(sender As Object, e As EventArgs) Handles btnSave1.Click
+
+        'input validation
         If Not ValidateInput() Then
             Exit Sub
         End If
 
+        'creating file which will held history of exchanges
         lstEntries1.Items.Add("United Kingdom -> " + lstCountries.SelectedItem + ";   " + txtAmount.Text + " => " + txtTotal.Text + " " + "(" + txtRate.Text + ")")
         Dim file As System.IO.StreamWriter
         file = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\11155882\Source\Repos\d345217\Currency-Converter\WindowsApp1\output1.txt", True)
         file.WriteLine("United Kingdom -> " + lstCountries.SelectedItem + ";   " + txtAmount.Text + " => " + txtTotal.Text + " " + "(" + txtRate.Text + ")")
         file.Close()
+
     End Sub
+
 End Class
--- WindowsApp1/Design2.vb
+++ WindowsApp1/Design2.vb
@@ -1,49 +1,68 @@
 ﻿Public Class Design2
+
+    'variable validation
     Dim Amount As Double
     Dim Result As Decimal
     Dim Currency As Decimal
     ReadOnly array(4, 1) As String
+
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ArrayCreate()
 
+        'filling listboxes with country names
         For i = 0 To 4
             lstFrom.Items.Add(array(i, 0))
             lstTo.Items.Add(array(i, 0))
         Next
 
+        'adding history to lstEntries
         Dim lines() As String = IO.File.ReadAllLines("C:\Users\11155882\Source\Repos\d345217\Currency-Converter\WindowsApp1\output2.txt")
         lstEntries2.Items.AddRange(lines)
+
     End Sub
     Private Sub btnCalculate2_Click(sender As Object, e As EventArgs) Handles btnCalculate2.Click
+
+        'input validation
         If Not ValidateInput() Then
             Exit Sub
         End If
 
         Calculate(lstFrom.SelectedIndex, lstTo.SelectedIndex)
+
     End Sub
     Private Sub ArrayCreate()
+
         'adding countries
         array(0, 0) = "United Kingdom"
         array(1, 0) = "Ukraine"
         array(2, 0) = "USA"
         array(3, 0) = "Poland"
         array(4, 0) = "Euro"
+
         'adding change rate to Hryvna
         array(0, 1) = 45.65
         array(1, 1) = 1
         array(2, 1) = 36.95
         array(3, 1) = 8.52
         array(4, 1) = 40.15
+
     End Sub
+
     Private Sub Calculate(ByRef x As Integer, y As Integer)
+
+        'calculations of conversion
         Amount = txtAmount.Text
         Result = Amount * array(x, 1) * (1 / array(y, 1))
-
         Currency = Result / Amount
+
+        'output to textbox
         txtCurrency.Text = Math.Round(Currency, 2)
         txtResult.Text = Math.Round(Result, 2)
+
     End Sub
+
     Private Function ValidateInput() As Boolean
+
         'check if amount is a positive number or exist
         If Not Double.TryParse(txtAmount.Text, Amount) OrElse Amount < 0 Then
             MessageBox.Show("Please enter a valid positive number for amount.")
@@ -57,16 +76,23 @@
         End If
 
         Return True
+
     End Function
+
     Private Sub btnSave2_Click(sender As Object, e As EventArgs) Handles btnSave2.Click
+
+        'input validation
         If Not ValidateInput() Then
             Exit Sub
         End If
 
+        'creating file which will held history of exchanges
         lstEntries2.Items.Add(lstFrom.SelectedItem + " -> " + lstTo.SelectedItem + ";   " + txtAmount.Text + " => " + txtResult.Text + " " + "(" + txtCurrency.Text + ")")
         Dim file As System.IO.StreamWriter
         file = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\11155882\Source\Repos\d345217\Currency-Converter\WindowsApp1\output2.txt", True)
         file.WriteLine(lstFrom.SelectedItem + " -> " + lstTo.SelectedItem + ";   " + txtAmount.Text + " => " + txtResult.Text + " " + "(" + txtCurrency.Text + ")")
         file.Close()
+
     End Sub
+
 End Class
